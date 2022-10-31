267d9692739b4999e3caed7771514b4a8cdc234c
Sayed Haider <sayedhaider300@outlook.com>
2022-10-31 10:50:16 +0530

    initial commit

---
+++ js/data.js
@@ -0,0 +1,729 @@
+const heroImagePath = "C:\\Users\\sayed\\OneDrive\\Desktop\\Personal\\Board\\Images\\Actors Manual Modified";
+
+const heroNameList = [
+    {
+        "id": 1,
+        "name": "Aamir Khan"
+    },
+    {
+        "id": 2,
+        "name": "Abhishek Bachchan"
+    },
+    {
+        "id": 3,
+        "name": "Ajay Devgan"
+    },
+    {
+        "id": 4,
+        "name": "Akshay Kumar"
+    },
+    {
+        "id": 5,
+        "name": "allu arjun"
+    },
+    {
+        "id": 6,
+        "name": "Amitabh Bacchan"
+    },
+    {
+        "id": 7,
+        "name": "Arjun Rampal"
+    },
+    {
+        "id": 8,
+        "name": "Prabhas"
+    },
+    {
+        "id": 9,
+        "name": "Ranbir Kapoor"
+    },
+    {
+        "id": 10,
+        "name": "Ranveer Singh"
+    },
+    {
+        "id": 11,
+        "name": "Saif Ali Khan"
+    },
+    {
+        "id": 12,
+        "name": "Salman Khan"
+    },
+    {
+        "id": 13,
+        "name": "Sanjay Dutt"
+    },
+    {
+        "id": 14,
+        "name": "Shahrukh Khan"
+    },
+    {
+        "id": 15,
+        "name": "Sonu Sood"
+    },
+    {
+        "id": 16,
+        "name": "Suniel Shetty"
+    },
+    {
+        "id": 17,
+        "name": "Vidyut Jammwal"
+    },
+    {
+        "id": 18,
+        "name": "Yash"
+    }
+]
+
+const allCard = [
+    {
+        "sn": 1,
+        "image": heroImagePath + "\\Aamir Khan Modified\\Aamir Khan1.jpg",
+        "heroMatchId": 1
+    },
+    {
+        "sn": 2,
+        "image": heroImagePath + "\\Aamir Khan Modified\\Aamir Khan10.jpg",
+        "heroMatchId": 1
+    },
+    {
+        "sn": 3,
+        "image": heroImagePath + "\\Aamir Khan Modified\\Aamir Khan11.jpg",
+        "heroMatchId": 1
+    },
+    {
+        "sn": 4,
+        "image": heroImagePath + "\\Aamir Khan Modified\\Aamir Khan12.jpg",
+        "heroMatchId": 1
+    },
+    {
+        "sn": 5,
+        "image": heroImagePath + "\\Aamir Khan Modified\\Aamir Khan13.jpg",
+        "heroMatchId": 1
+    },
+    {
+        "sn": 6,
+        "image": heroImagePath + "\\Aamir Khan Modified\\Aamir Khan14.jpg",
+        "heroMatchId": 1
+    },
+    {
+        "sn": 7,
+        "image": heroImagePath + "\\Aamir Khan Modified\\Aamir Khan15.jpg",
+        "heroMatchId": 1
+    },
+    {
+        "sn": 8,
+        "image": heroImagePath + "\\Aamir Khan Modified\\Aamir Khan16.jpg",
+        "heroMatchId": 1
+    },
+    {
+        "sn": 9,
+        "image": heroImagePath + "\\Aamir Khan Modified\\Aamir Khan17.jpg",
+        "heroMatchId": 1
+    },
+    {
+        "sn": 10,
+        "image": heroImagePath + "\\Aamir Khan Modified\\Aamir Khan18.jpg",
+        "heroMatchId": 1
+    },
+    {
+        "sn": 11,
+        "image": heroImagePath + "\\Aamir Khan Modified\\Aamir Khan19.jpg",
+        "heroMatchId": 1
+    },
+    {
+        "sn": 12,
+        "image": heroImagePath + "\\Aamir Khan Modified\\Aamir Khan2.jpg",
+        "heroMatchId": 1
+    },
+    {
+        "sn": 13,
+        "image": heroImagePath + "\\Aamir Khan Modified\\Aamir Khan20.jpg",
+        "heroMatchId": 1
+    },
+    {
+        "sn": 14,
+        "image": heroImagePath + "\\Aamir Khan Modified\\Aamir Khan21.jpg",
+        "heroMatchId": 1
+    },
+    {
+        "sn": 15,
+        "image": heroImagePath + "\\Aamir Khan Modified\\Aamir Khan22.jpg",
+        "heroMatchId": 1
+    },
+    {
+        "sn": 16,
+        "image": heroImagePath + "\\Aamir Khan Modified\\Aamir Khan23.jpg",
+        "heroMatchId": 1
+    },
+    {
+        "sn": 17,
+        "image": heroImagePath + "\\Aamir Khan Modified\\Aamir Khan24.jpg",
+        "heroMatchId": 1
+    },
+    {
+        "sn": 18,
+        "image": heroImagePath + "\\Aamir Khan Modified\\Aamir Khan25.jpg",
+        "heroMatchId": 1
+    },
+    {
+        "sn": 19,
+        "image": heroImagePath + "\\Aamir Khan Modified\\Aamir Khan26.jpg",
+        "heroMatchId": 1
+    },
+    {
+        "sn": 20,
+        "image": heroImagePath + "\\Aamir Khan Modified\\Aamir Khan27.jpg",
+        "heroMatchId": 1
+    },
+    {
+        "sn": 21,
+        "image": heroImagePath + "\\Aamir Khan Modified\\Aamir Khan3.jpg",
+        "heroMatchId": 1
+    },
+    {
+        "sn": 22,
+        "image": heroImagePath + "\\Aamir Khan Modified\\Aamir Khan4.jpg",
+        "heroMatchId": 1
+    },
+    {
+        "sn": 23,
+        "image": heroImagePath + "\\Aamir Khan Modified\\Aamir Khan5.jpg",
+        "heroMatchId": 1
+    },
+    {
+        "sn": 24,
+        "image": heroImagePath + "\\Aamir Khan Modified\\Aamir Khan6.jpg",
+        "heroMatchId": 1
+    },
+    {
+        "sn": 25,
+        "image": heroImagePath + "\\Aamir Khan Modified\\Aamir Khan7.jpg",
+        "heroMatchId": 1
+    },
+    {
+        "sn": 26,
+        "image": heroImagePath + "\\Aamir Khan Modified\\Aamir Khan8.jpg",
+        "heroMatchId": 1
+    },
+    {
+        "sn": 27,
+        "image": heroImagePath + "\\Aamir Khan Modified\\Aamir Khan9.jpg",
+        "heroMatchId": 1
+    },
+    {
+        "sn": 28,
+        "image": heroImagePath + "\\Abhishek Bachchan Modified\\Abhishek Bachchan1.jpg",
+        "heroMatchId": 2
+    },
+    {
+        "sn": 29,
+        "image": heroImagePath + "\\Abhishek Bachchan Modified\\Abhishek Bachchan2.jpg",
+        "heroMatchId": 2
+    },
+    {
+        "sn": 30,
+        "image": heroImagePath + "\\Abhishek Bachchan Modified\\Abhishek Bachchan3.jpg",
+        "heroMatchId": 2
+    },
+    {
+        "sn": 31,
+        "image": heroImagePath + "\\Ajay Devgan Modified\\Ajay Devgan1.jpg",
+        "heroMatchId": 3
+    },
+    {
+        "sn": 32,
+        "image": heroImagePath + "\\Ajay Devgan Modified\\Ajay Devgan2.jpg",
+        "heroMatchId": 3
+    },
+    {
+        "sn": 33,
+        "image": heroImagePath + "\\Ajay Devgan Modified\\Ajay Devgan3.jpg",
+        "heroMatchId": 3
+    },
+    {
+        "sn": 34,
+        "image": heroImagePath + "\\Ajay Devgan Modified\\Ajay Devgan4.jpg",
+        "heroMatchId": 3
+    },
+    {
+        "sn": 35,
+        "image": heroImagePath + "\\Ajay Devgan Modified\\Ajay Devgan5.jpg",
+        "heroMatchId": 3
+    },
+    {
+        "sn": 36,
+        "image": heroImagePath + "\\Ajay Devgan Modified\\Ajay Devgan6.jpg",
+        "heroMatchId": 3
+    },
+    {
+        "sn": 37,
+        "image": heroImagePath + "\\Akshay Kumar Modified\\Akshay Kumar1.jpg",
+        "heroMatchId": 4
+    },
+    {
+        "sn": 38,
+        "image": heroImagePath + "\\Akshay Kumar Modified\\Akshay Kumar10.jpg",
+        "heroMatchId": 4
+    },
+    {
+        "sn": 39,
+        "image": heroImagePath + "\\Akshay Kumar Modified\\Akshay Kumar11.jpg",
+        "heroMatchId": 4
+    },
+    {
+        "sn": 40,
+        "image": heroImagePath + "\\Akshay Kumar Modified\\Akshay Kumar12.jpg",
+        "heroMatchId": 4
+    },
+    {
+        "sn": 41,
+        "image": heroImagePath + "\\Akshay Kumar Modified\\Akshay Kumar13.jpg",
+        "heroMatchId": 4
+    },
+    {
+        "sn": 42,
+        "image": heroImagePath + "\\Akshay Kumar Modified\\Akshay Kumar14.jpg",
+        "heroMatchId": 4
+    },
+    {
+        "sn": 43,
+        "image": heroImagePath + "\\Akshay Kumar Modified\\Akshay Kumar15.jpg",
+        "heroMatchId": 4
+    },
+    {
+        "sn": 44,
+        "image": heroImagePath + "\\Akshay Kumar Modified\\Akshay Kumar2.jpg",
+        "heroMatchId": 4
+    },
+    {
+        "sn": 45,
+        "image": heroImagePath + "\\Akshay Kumar Modified\\Akshay Kumar3.jpg",
+        "heroMatchId": 4
+    },
+    {
+        "sn": 46,
+        "image": heroImagePath + "\\Akshay Kumar Modified\\Akshay Kumar4.jpg",
+        "heroMatchId": 4
+    },
+    {
+        "sn": 47,
+        "image": heroImagePath + "\\Akshay Kumar Modified\\Akshay Kumar5.jpg",
+        "heroMatchId": 4
+    },
+    {
+        "sn": 48,
+        "image": heroImagePath + "\\Akshay Kumar Modified\\Akshay Kumar6.jpg",
+        "heroMatchId": 4
+    },
+    {
+        "sn": 49,
+        "image": heroImagePath + "\\Akshay Kumar Modified\\Akshay Kumar7.jpg",
+        "heroMatchId": 4
+    },
+    {
+        "sn": 50,
+        "image": heroImagePath + "\\Akshay Kumar Modified\\Akshay Kumar8.jpg",
+        "heroMatchId": 4
+    },
+    {
+        "sn": 51,
+        "image": heroImagePath + "\\Akshay Kumar Modified\\Akshay Kumar9.jpg",
+        "heroMatchId": 4
+    },
+    {
+        "sn": 52,
+        "image": heroImagePath + "\\allu arjun Modified\\allu arjun1.jpg",
+        "heroMatchId": 5
+    },
+    {
+        "sn": 53,
+        "image": heroImagePath + "\\allu arjun Modified\\allu arjun2.jpg",
+        "heroMatchId": 5
+    },
+    {
+        "sn": 54,
+        "image": heroImagePath + "\\allu arjun Modified\\allu arjun3.jpg",
+        "heroMatchId": 5
+    },
+    {
+        "sn": 55,
+        "image": heroImagePath + "\\allu arjun Modified\\allu arjun4.jpg",
+        "heroMatchId": 5
+    },
+    {
+        "sn": 56,
+        "image": heroImagePath + "\\Amitabh Bacchan Modified\\Amitabh Bacchan1.jpg",
+        "heroMatchId": 6
+    },
+    {
+        "sn": 57,
+        "image": heroImagePath + "\\Amitabh Bacchan Modified\\Amitabh Bacchan2.jpg",
+        "heroMatchId": 6
+    },
+    {
+        "sn": 58,
+        "image": heroImagePath + "\\Amitabh Bacchan Modified\\Amitabh Bacchan3.jpg",
+        "heroMatchId": 6
+    },
+    {
+        "sn": 59,
+        "image": heroImagePath + "\\Amitabh Bacchan Modified\\Amitabh Bacchan4.jpg",
+        "heroMatchId": 6
+    },
+    {
+        "sn": 60,
+        "image": heroImagePath + "\\Amitabh Bacchan Modified\\Amitabh Bacchan5.jpg",
+        "heroMatchId": 6
+    },
+    {
+        "sn": 61,
+        "image": heroImagePath + "\\Arjun Rampal Modified\\Arjun Rampal1.jpg",
+        "heroMatchId": 7
+    },
+    {
+        "sn": 62,
+        "image": heroImagePath + "\\Arjun Rampal Modified\\Arjun Rampal2.jpg",
+        "heroMatchId": 7
+    },
+    {
+        "sn": 63,
+        "image": heroImagePath + "\\Arjun Rampal Modified\\Arjun Rampal3.jpg",
+        "heroMatchId": 7
+    },
+    {
+        "sn": 64,
+        "image": heroImagePath + "\\Prabhas Modified\\Prabhas1.jpg",
+        "heroMatchId": 8
+    },
+    {
+        "sn": 65,
+        "image": heroImagePath + "\\Prabhas Modified\\Prabhas2.jpg",
+        "heroMatchId": 8
+    },
+    {
+        "sn": 66,
+        "image": heroImagePath + "\\Prabhas Modified\\Prabhas3.jpg",
+        "heroMatchId": 8
+    },
+    {
+        "sn": 67,
+        "image": heroImagePath + "\\Ranbir Kapoor Modified\\Ranbir Kapoor1.jpg",
+        "heroMatchId": 9
+    },
+    {
+        "sn": 68,
+        "image": heroImagePath + "\\Ranbir Kapoor Modified\\Ranbir Kapoor2.jpg",
+        "heroMatchId": 9
+    },
+    {
+        "sn": 69,
+        "image": heroImagePath + "\\Ranveer Singh Modified\\Ranveer Singh1.jpg",
+        "heroMatchId": 10
+    },
+    {
+        "sn": 70,
+        "image": heroImagePath + "\\Ranveer Singh Modified\\Ranveer Singh2.jpg",
+        "heroMatchId": 10
+    },
+    {
+        "sn": 71,
+        "image": heroImagePath + "\\Ranveer Singh Modified\\Ranveer Singh3.jpg",
+        "heroMatchId": 10
+    },
+    {
+        "sn": 72,
+        "image": heroImagePath + "\\Saif Ali Khan Modified\\Saif Ali Khan1.jpg",
+        "heroMatchId": 11
+    },
+    {
+        "sn": 73,
+        "image": heroImagePath + "\\Saif Ali Khan Modified\\Saif Ali Khan2.jpg",
+        "heroMatchId": 11
+    },
+    {
+        "sn": 74,
+        "image": heroImagePath + "\\Saif Ali Khan Modified\\Saif Ali Khan3.jpg",
+        "heroMatchId": 11
+    },
+    {
+        "sn": 75,
+        "image": heroImagePath + "\\Saif Ali Khan Modified\\Saif Ali Khan4.jpg",
+        "heroMatchId": 11
+    },
+    {
+        "sn": 76,
+        "image": heroImagePath + "\\Saif Ali Khan Modified\\Saif Ali Khan5.jpg",
+        "heroMatchId": 11
+    },
+    {
+        "sn": 77,
+        "image": heroImagePath + "\\Salman Khan Modified\\Salman Khan1.jpg",
+        "heroMatchId": 12
+    },
+    {
+        "sn": 78,
+        "image": heroImagePath + "\\Salman Khan Modified\\Salman Khan2.jpg",
+        "heroMatchId": 12
+    },
+    {
+        "sn": 79,
+        "image": heroImagePath + "\\Salman Khan Modified\\Salman Khan3.jpg",
+        "heroMatchId": 12
+    },
+    {
+        "sn": 80,
+        "image": heroImagePath + "\\Salman Khan Modified\\Salman Khan4.jpg",
+        "heroMatchId": 12
+    },
+    {
+        "sn": 81,
+        "image": heroImagePath + "\\Salman Khan Modified\\Salman Khan5.jpg",
+        "heroMatchId": 12
+    },
+    {
+        "sn": 82,
+        "image": heroImagePath + "\\Salman Khan Modified\\Salman Khan6.jpg",
+        "heroMatchId": 12
+    },
+    {
+        "sn": 83,
+        "image": heroImagePath + "\\Salman Khan Modified\\Salman Khan7.jpg",
+        "heroMatchId": 12
+    },
+    {
+        "sn": 84,
+        "image": heroImagePath + "\\Salman Khan Modified\\Salman Khan8.jpg",
+        "heroMatchId": 12
+    },
+    {
+        "sn": 85,
+        "image": heroImagePath + "\\Salman Khan Modified\\Salman Khan9.jpg",
+        "heroMatchId": 12
+    },
+    {
+        "sn": 86,
+        "image": heroImagePath + "\\Sanjay Dutt Modified\\Sanjay Dutt1.jpg",
+        "heroMatchId": 13
+    },
+    {
+        "sn": 87,
+        "image": heroImagePath + "\\Sanjay Dutt Modified\\Sanjay Dutt2.jpg",
+        "heroMatchId": 13
+    },
+    {
+        "sn": 88,
+        "image": heroImagePath + "\\Sanjay Dutt Modified\\Sanjay Dutt3.jpg",
+        "heroMatchId": 13
+    },
+    {
+        "sn": 89,
+        "image": heroImagePath + "\\Sanjay Dutt Modified\\Sanjay Dutt4.jpg",
+        "heroMatchId": 13
+    },
+    {
+        "sn": 90,
+        "image": heroImagePath + "\\Sanjay Dutt Modified\\Sanjay Dutt5.jpg",
+        "heroMatchId": 13
+    },
+    {
+        "sn": 91,
+        "image": heroImagePath + "\\Shahrukh Khan Modified\\Shahrukh Khan1.jpg",
+        "heroMatchId": 14
+    },
+    {
+        "sn": 92,
+        "image": heroImagePath + "\\Shahrukh Khan Modified\\Shahrukh Khan10.jpg",
+        "heroMatchId": 14
+    },
+    {
+        "sn": 93,
+        "image": heroImagePath + "\\Shahrukh Khan Modified\\Shahrukh Khan11.jpg",
+        "heroMatchId": 14
+    },
+    {
+        "sn": 94,
+        "image": heroImagePath + "\\Shahrukh Khan Modified\\Shahrukh Khan12.jpg",
+        "heroMatchId": 14
+    },
+    {
+        "sn": 95,
+        "image": heroImagePath + "\\Shahrukh Khan Modified\\Shahrukh Khan13.jpg",
+        "heroMatchId": 14
+    },
+    {
+        "sn": 96,
+        "image": heroImagePath + "\\Shahrukh Khan Modified\\Shahrukh Khan14.jpg",
+        "heroMatchId": 14
+    },
+    {
+        "sn": 97,
+        "image": heroImagePath + "\\Shahrukh Khan Modified\\Shahrukh Khan15.jpg",
+        "heroMatchId": 14
+    },
+    {
+        "sn": 98,
+        "image": heroImagePath + "\\Shahrukh Khan Modified\\Shahrukh Khan16.jpg",
+        "heroMatchId": 14
+    },
+    {
+        "sn": 99,
+        "image": heroImagePath + "\\Shahrukh Khan Modified\\Shahrukh Khan17.jpg",
+        "heroMatchId": 14
+    },
+    {
+        "sn": 100,
+        "image": heroImagePath + "\\Shahrukh Khan Modified\\Shahrukh Khan18.jpg",
+        "heroMatchId": 14
+    },
+    {
+        "sn": 101,
+        "image": heroImagePath + "\\Shahrukh Khan Modified\\Shahrukh Khan19.jpg",
+        "heroMatchId": 14
+    },
+    {
+        "sn": 102,
+        "image": heroImagePath + "\\Shahrukh Khan Modified\\Shahrukh Khan2.jpg",
+        "heroMatchId": 14
+    },
+    {
+        "sn": 103,
+        "image": heroImagePath + "\\Shahrukh Khan Modified\\Shahrukh Khan20.jpg",
+        "heroMatchId": 14
+    },
+    {
+        "sn": 104,
+        "image": heroImagePath + "\\Shahrukh Khan Modified\\Shahrukh Khan21.jpg",
+        "heroMatchId": 14
+    },
+    {
+        "sn": 105,
+        "image": heroImagePath + "\\Shahrukh Khan Modified\\Shahrukh Khan22.jpg",
+        "heroMatchId": 14
+    },
+    {
+        "sn": 106,
+        "image": heroImagePath + "\\Shahrukh Khan Modified\\Shahrukh Khan23.jpg",
+        "heroMatchId": 14
+    },
+    {
+        "sn": 107,
+        "image": heroImagePath + "\\Shahrukh Khan Modified\\Shahrukh Khan24.jpg",
+        "heroMatchId": 14
+    },
+    {
+        "sn": 108,
+        "image": heroImagePath + "\\Shahrukh Khan Modified\\Shahrukh Khan25.jpg",
+        "heroMatchId": 14
+    },
+    {
+        "sn": 109,
+        "image": heroImagePath + "\\Shahrukh Khan Modified\\Shahrukh Khan26.jpg",
+        "heroMatchId": 14
+    },
+    {
+        "sn": 110,
+        "image": heroImagePath + "\\Shahrukh Khan Modified\\Shahrukh Khan27.jpg",
+        "heroMatchId": 14
+    },
+    {
+        "sn": 111,
+        "image": heroImagePath + "\\Shahrukh Khan Modified\\Shahrukh Khan28.jpg",
+        "heroMatchId": 14
+    },
+    {
+        "sn": 112,
+        "image": heroImagePath + "\\Shahrukh Khan Modified\\Shahrukh Khan29.jpg",
+        "heroMatchId": 14
+    },
+    {
+        "sn": 113,
+        "image": heroImagePath + "\\Shahrukh Khan Modified\\Shahrukh Khan3.jpg",
+        "heroMatchId": 14
+    },
+    {
+        "sn": 114,
+        "image": heroImagePath + "\\Shahrukh Khan Modified\\Shahrukh Khan30.jpg",
+        "heroMatchId": 14
+    },
+    {
+        "sn": 115,
+        "image": heroImagePath + "\\Shahrukh Khan Modified\\Shahrukh Khan4.jpg",
+        "heroMatchId": 14
+    },
+    {
+        "sn": 116,
+        "image": heroImagePath + "\\Shahrukh Khan Modified\\Shahrukh Khan5.jpg",
+        "heroMatchId": 14
+    },
+    {
+        "sn": 117,
+        "image": heroImagePath + "\\Shahrukh Khan Modified\\Shahrukh Khan6.jpg",
+        "heroMatchId": 14
+    },
+    {
+        "sn": 118,
+        "image": heroImagePath + "\\Shahrukh Khan Modified\\Shahrukh Khan7.jpg",
+        "heroMatchId": 14
+    },
+    {
+        "sn": 119,
+        "image": heroImagePath + "\\Shahrukh Khan Modified\\Shahrukh Khan8.jpg",
+        "heroMatchId": 14
+    },
+    {
+        "sn": 120,
+        "image": heroImagePath + "\\Shahrukh Khan Modified\\Shahrukh Khan9.jpg",
+        "heroMatchId": 14
+    },
+    {
+        "sn": 121,
+        "image": heroImagePath + "\\Sonu Sood Modified\\Sonu Sood1.jpg",
+        "heroMatchId": 15
+    },
+    {
+        "sn": 122,
+        "image": heroImagePath + "\\Sonu Sood Modified\\Sonu Sood2.jpg",
+        "heroMatchId": 15
+    },
+    {
+        "sn": 123,
+        "image": heroImagePath + "\\Suniel Shetty Modified\\Suniel Shetty1.jpg",
+        "heroMatchId": 16
+    },
+    {
+        "sn": 124,
+        "image": heroImagePath + "\\Suniel Shetty Modified\\Suniel Shetty2.jpg",
+        "heroMatchId": 16
+    },
+    {
+        "sn": 125,
+        "image": heroImagePath + "\\Suniel Shetty Modified\\Suniel Shetty3.jpg",
+        "heroMatchId": 16
+    },
+    {
+        "sn": 126,
+        "image": heroImagePath + "\\Suniel Shetty Modified\\Suniel Shetty4.jpg",
+        "heroMatchId": 16
+    },
+    {
+        "sn": 127,
+        "image": heroImagePath + "\\Vidyut Jammwal Modified\\Vidyut Jammwal1.jpg",
+        "heroMatchId": 17
+    },
+    {
+        "sn": 128,
+        "image": heroImagePath + "\\Vidyut Jammwal Modified\\Vidyut Jammwal2.jpg",
+        "heroMatchId": 17
+    },
+    {
+        "sn": 129,
+        "image": heroImagePath + "\\Yash Modified\\Yash1.jpg",
+        "heroMatchId": 18
+    },
+    {
+        "sn": 130,
+        "image": heroImagePath + "\\Yash Modified\\Yash2.jpg",
+        "heroMatchId": 18
+    }
+]
